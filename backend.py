155f36056a7ae9b23cff27670cc995636fb9ed4d
Lubayna <youusenn1994@gmail.com>
2023-02-24 00:49:52 +0900

    second version

--- backend.py
+++ backend.py
@@ -1,46 +1,65 @@
-from flask import Flask, render_template,request, jsonify
+from flask import Flask, render_template,request, jsonify, abort, Response
 import numpy as np
 import json
 import matplotlib as mpl
 from pylab import *
 import sys
+from flask_cors import CORS
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"])
+@app.route('/satori/julia', methods=["GET"])
 def index():
     return render_template('index.html')
-
-@app.route('/julia', methods=["GET"])
+#CORS(app)
+@app.route('/', methods=["POST"])
 def julia_result_post():
     # POST送信の処理
-    min_x = request.args.get('min_x', type=float)
-    max_x = request.args.get('max_x', type=float)
-    min_y = request.args.get('min_y', type=float)
-    max_y = request.args.get('max_y', type=float)
-    colormap = request.args.get('colormap', type=str)
-    width = request.args.get('width', type=int)
-    height = request.args.get('height', type=int)
-    max_iter = request.args.get('max_iter', type=int)
-    comp_const = request.args.get('comp_const', type=str)
-    comp_x, comp_y = list(map(float, comp_const.rstrip('\r\n').split(',')))
-  #  width, height, max_iter = 800, 600, 100
-    x = np.linspace(min_x, max_x, width)
-    y = np.linspace(min_y, max_y, height)
-    z = x + y[:, np.newaxis] * 1j
-    m = np.zeros((height, width))
-    for i in range(max_iter):
-        z = z * z + complex(comp_x,comp_y)
-        m[np.abs(z) > 2] = i
-    color_bar = []
-    cmap = cm.get_cmap(colormap, max_iter)  # PiYG
-    for i in range(cmap.N):
-        rgba = cmap(i)
-       # print(matplotlib.colors.rgb2hex(rgba), file=sys.stderr)
-        color_bar.append(int(mpl.colors.rgb2hex(rgba)[1:], base=16))
+    try:
+        data = request.json
+        min_x = data.get('min_x')
+        max_x = data.get('max_x')
+        min_y = data.get('min_y')
+        max_y = data.get('max_y')
+        colormap = data.get('colormap')
+        width = data.get('width')
+        height = data.get('height')
+        max_iter = data.get('max_iter')
+        comp_const = data.get('comp_const')
+        comp_x = comp_const[0]
+        comp_y = comp_const[1]
+        if min_x == None: return jsonify({'error': '最小実数部は数値でなければなりません'})
+        if max_x == None: return jsonify({'error': '最大実数部は数値でなければなりません'})
+        if min_y == None: return jsonify({'error': '最小虚数部は数値でなければなりません'})
+        if max_y == None: return jsonify({'error': '最大虚数部は数値でなければなりません'})
+        if width == None: return jsonify({'error': '幅は整数でなければなりません'})
+        if height == None: return jsonify({'error': '高さは整数でなければなりません'})
+        if max_iter == None: return jsonify({'error': '反復回数は整数でなければなりません'})
+        if comp_x == None: return jsonify({'error': '複素定数実数部は数値でなければなりません'})
+        if comp_y == None: return jsonify({'error': '複素定数虚数部は数値でなければなりません'})
+        
+        if min_x >= max_x:
+            return jsonify({'error': '最小実数部は最大実数部より小さくなければなりません'})
+        if min_y >= max_y:
+            return jsonify({'error': '最小虚数部は最大虚数部より小さくなければなりません'})
 
-    return render_template('result.html', julia_set=json.dumps(m.tolist()), colors=json.dumps(color_bar), width=width, height=height, max_iter=max_iter)#, plot_url=plot_url)
+        x = np.linspace(min_x, max_x, width)
+        y = np.linspace(min_y, max_y, height)
+        z = x + y[:, np.newaxis] * 1j
+        m = np.zeros((height, width))
+        for i in range(max_iter):
+            z = z * z + complex(comp_x, comp_y)
+            m[np.abs(z) > 2] = i
+        color_bar = []
+        cmap = cm.get_cmap(colormap, max_iter)  # PiYG
+        for i in range(cmap.N):
+            rgba = cmap(i)
+            color_bar.append(int(mpl.colors.rgb2hex(rgba)[1:], base=16))
 
+        return jsonify({ "width": width, "height": height, "julia_set": json.dumps(m.tolist()), "colors": json.dumps(color_bar) })
+        # return jsonify({'image': image_str})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.debug = False
-    app.run(port=4000, host='localhost')
+    app.run(port=8080, host='localhost')
---
+++ static/scripts/myscript.js
@@ -0,0 +1,76 @@
+async function clickedButton() {
+    const data = await getJuliaData()
+    if (data.error) {
+        alert(`エラー: ${data.error}`)
+        return
+    }
+    const canvas = createCanvas(data)
+    addCanvas(canvas)
+}
+
+function addCanvas(canvas) {
+    const old = document.getElementById("juliaDiagram")
+    if (old !== null) {
+        old.remove()
+    }
+    const c = document.createElement('div')
+    c.setAttribute("id", "juliaDiagram")
+    //c.classList.add("sideBySide")
+    c.appendChild(canvas)
+    document.getElementById("presenter").appendChild(c)
+}
+
+async function getJuliaData() {
+    const min_x = Number(document.getElementById("min_x").value)
+    const max_x = Number(document.getElementById("max_x").value)
+    const min_y = Number(document.getElementById("min_y").value)
+    const max_y = Number(document.getElementById("max_y").value)
+    const comp_const = JSON.parse("[" + document.getElementById("comp_const").value + "]")
+    const max_iter = Number(document.getElementById("max_iter").value)
+    const width = Number(document.getElementById("width").value)
+    const height = Number(document.getElementById("height").value)
+    const colormap = document.getElementById("colormap").value
+
+    return await fetch("http://localhost:8080/", {
+        method: 'POST',
+        mode: "cors",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            min_x,
+            max_x,
+            min_y,
+            max_y,
+            comp_const,
+            max_iter,
+            width,
+            height,
+            colormap
+        })
+    }).then((response) => response.json())
+    .catch((error) => {
+        alert(`エラー: ${error}`)
+    })
+}
+
+function createCanvas(data) {
+
+    const canvas = document.createElement('canvas')
+    var julia_matrix = JSON.parse(data.julia_set)
+    var colors = JSON.parse(data.colors)
+    canvas.width = data.width
+    canvas.height = data.height
+    const ctx = canvas.getContext('2d')
+    for (let i = 0; i < julia_matrix.length; i++) {
+        julia_arr = julia_matrix[i]
+        for (let j = 0; j < julia_arr.length; j++) {
+            julia_num = julia_arr[j]
+            var color = colors[julia_num]
+            ctx.fillStyle = `#${color.toString(16)}`
+            ctx.fillRect(j,i,1,1)
+        }
+    }
+
+    return canvas
+}
